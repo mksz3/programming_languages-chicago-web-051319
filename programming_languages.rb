0a308a3e7ae944fc1afc56ea7b2aa4be9feedac9
Markus Sanders <markus.sanderst@gmail.com>
2019-05-04 21:30:26 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,12 +1,12 @@
 def reformat_languages(languages)
-  languages.each { |type|
-    if type
-  }
-  oo = languages[:oo].each { |language, info| 
-    info[:style] = [:oo]
-  }
-  functional = languages[:functional].each { |language, info| 
-    info[:style] = [:functional]
-  }
-  oo.merge(functional)
+  # languages.each { |type|
+  #   if type
+  # }
+  # oo = languages[:oo].each { |language, info| 
+  #   info[:style] = [:oo]
+  # }
+  # functional = languages[:functional].each { |language, info| 
+  #   info[:style] = [:functional]
+  # }
+  # oo.merge(functional)
 end
