229d454c865d2c0497250df7646d6b8dcc67d825
Markus Sanders <markus.sanderst@gmail.com>
2019-05-04 20:30:41 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,68 +6,68 @@ def reformat_languages(languages)
   }
 end
 
-```ruby
-languages = {
-  :oo => {
-    :ruby => {
-      :type => "interpreted"
-    },
-    :javascript => {
-      :type => "interpreted"
-    },
-    :python => {
-      :type => "interpreted"
-    },
-    :java => {
-      :type => "compiled"
-    }
-  },
-  :functional => {
-    :clojure => {
-      :type => "compiled"
-    },
-    :erlang => {
-      :type => "compiled"
-    },
-    :scala => {
-      :type => "compiled"
-    },
-    :javascript => {
-      :type => "interpreted"
-    }
+# ```ruby
+# languages = {
+#   :oo => {
+#     :ruby => {
+#       :type => "interpreted"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     },
+#     :python => {
+#       :type => "interpreted"
+#     },
+#     :java => {
+#       :type => "compiled"
+#     }
+#   },
+#   :functional => {
+#     :clojure => {
+#       :type => "compiled"
+#     },
+#     :erlang => {
+#       :type => "compiled"
+#     },
+#     :scala => {
+#       :type => "compiled"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     }
  
-  }
-}
-```
-```ruby
-{
-  :ruby => {
-    :type => "interpreted",
-    :style => [:oo]
-  },
-  :javascript => {
-    :type => "interpreted",
-    :style => [:oo, :functional]
-  },
-  :python => {
-    :type => "interpreted",
-    :style => [:oo]
-  },
-  :java => {
-    :type => "compiled",
-    :style => [:oo]
-  },
-  :clojure => {
-    :type => "compiled",
-    :style => [:functional]
-  },
-  :erlang => {
-    :type => "compiled",
-    :style => [:functional]
-  },
-  :scala => {
-    :type => "compiled",
-    :style => [:functional]
-  }
-}
-```
+#   }
+# }
+# ```
+# ```ruby
+# {
+#   :ruby => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :javascript => {
+#     :type => "interpreted",
+#     :style => [:oo, :functional]
+#   },
+#   :python => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :java => {
+#     :type => "compiled",
+#     :style => [:oo]
+#   },
+#   :clojure => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   },
+#   :erlang => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   },
+#   :scala => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   }
+# }
+# ```
