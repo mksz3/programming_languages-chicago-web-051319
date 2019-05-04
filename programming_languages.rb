110d77235a53c57ce277ca52baf0fe6bad0a7393
Markus Sanders <markus.sanderst@gmail.com>
2019-05-04 21:31:59 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,22 @@
 def reformat_languages(languages)
-  # your code here
+  ##### doesn't merge duplicate languages #####
+  # languages.each { |type|
+  #   if type
+  # }
+  # oo = languages[:oo].each { |language, info| 
+  #   info[:style] = [:oo]
+  # }
+  # functional = languages[:functional].each { |language, info| 
+  #   info[:style] = [:functional]
+  # }
+  # oo.merge(functional)
+  #########################
+  
+  formatted = {}
+  languages.each { |style, language|
+      language.each { |name, att|
+          formatted[name] ? formatted[name][:style] << style : formatted[name] = {type: att[:type], style: [style]}
+      }
+  }
+  formatted
 end
